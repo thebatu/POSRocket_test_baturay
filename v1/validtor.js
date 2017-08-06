d1af344b3abc1e363cd61df66d0cefb031220cb0
baturay alhaj ahmad <thebatu@gmail.com>
2017-08-06 02:34:26 +0200

    first commit

---
+++ v1/validtor.js
@@ -0,0 +1,22 @@
+var fs = require("fs")
+
+var content = fs.readFileSync("transaction.json")
+
+var l = { "business_id" : function(x){return (x=="12")},
+          "location_id" : function(x){return (x=="14")},
+}
+
+
+//{key:function()}
+function validate(lis,j){
+    var test = JSON.parse(j)
+
+    for(var c in lis){
+       if(!(lis[c](test[c]))){
+           return false
+        }
+    }
+    return true
+}
+
+console.log(validate(l,content))
